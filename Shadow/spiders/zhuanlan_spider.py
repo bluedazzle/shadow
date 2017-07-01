02e71b97221950df03e8e94b9896c0aa88258b6d
rapospectre <rapospectre@163.com>
2017-07-01 16:54:25 +0800

    <fix>  fix error
    Author:rapospectre@gmail.com
    
    Desc: fix error

--- Shadow/spiders/zhuanlan_spider.py
+++ Shadow/spiders/zhuanlan_spider.py
@@ -218,9 +218,7 @@ class ZhuanLanSpider(scrapy.Spider):
         yield Request(url, headers=headers, callback=self.parse_api_result)
         url = self.column_api_url.format(slug=self.get_zhuanlan_name())
         yield Request(url, headers=headers, callback=self.parse_column_info)
-        self.modify_obj()
-
-    # self.modify_obj()
+        # self.modify_obj()
 
     def parse_column_info(self, response):
         data = json.loads(response.body)
